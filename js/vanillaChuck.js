a8942f518baba55d5f0fa5bfc28125f7953fb70f
danilo.carrabino <danilo.carrabino@gmail.com>
2017-11-05 22:51:58 +0100

    Changed Chuck Norris image and added DanJsonp class in a separate file

--- js/vanillaChuck.js
+++ js/vanillaChuck.js
@@ -1,54 +1,9 @@
 'use strict';
 
 /**
- * A class to implement JSONP protocol inside a web page
- * 
- * @returns {null | Promise} returns a Promise if no callbackName is provided, null elsewhere
+ * A class to invoke Chuck Norris random jokes API offered by https://api.icndb.com
+ * This class uses DanJsonp library to perform the JSONP protocol
  */
-class DanJsonp {
-    static invokeJsonp(uri, callbackName = null) {
-        // create the script dom node, which will contain the address to be invoked
-        let s = document.createElement("script");
-        let separator = uri.indexOf('?') !== -1 ? "&" : "?";
-        let finalAddress = uri;
-
-        if(callbackName) {
-            finalAddress += separator + "callback=" + callbackName;
-            s.src = finalAddress;
-
-            // append script node to document body
-            document.body.appendChild(s);
-        }
-        else {
-            // create the promise to return
-            var willPromiseFinish = new Promise(
-                (resolve, reject) => {
-                    // assign static method danJsonpCallback to class DanJsonp
-                    Object.assign(DanJsonp, {
-                        danJsonpCallback(myObj) {
-                            // resolve promise after callback invocation
-                            if(myObj) {
-                                resolve(myObj);
-                            }
-                            else {
-                                reject("No value returned by server");
-                            }
-                        }
-                    });
-                }
-            );
-
-            finalAddress += separator + "callback=DanJsonp.danJsonpCallback";
-            s.src = finalAddress;
-
-            // append script node to document body
-            document.body.appendChild(s);
-
-            return willPromiseFinish;
-        }
-    }
-}
-
 class IcndbAPI {
     static baseApiAddress() {
         return "https://api.icndb.com";
